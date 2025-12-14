7a1200a49eae6a8c88d170f78d2a5c613d460c76
Venkata4507 <Venkat@techdhaba.com>
2025-12-14 15:38:51 +0530

    example or practice

---
+++ ex.c
@@ -0,0 +1,12 @@
+/*
+Name:ATTIPATI VENKATA RAMANAIAH
+Date:
+Title:
+Sample I/P:
+Sample O/p:
+*/
+#include<stdio.h>
+int main()
+{
+    printf("hello\n");
+}
---
+++ ex1.c
@@ -0,0 +1,16 @@
+/*
+Name:ATTIPATI VENKATA RAMANAIAH
+Date:
+Title:
+Sample I/P:
+Sample O/p:
+*/
+#include<stdio.h>
+int main()
+{
+
+
+
+
+
+}
